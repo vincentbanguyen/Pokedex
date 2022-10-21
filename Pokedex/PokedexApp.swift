f5ee71ee0334caeecbace5d14d77c1b915f4b652
Vincent Nguyen <vincentbanguyen@gmail.com>
2022-10-21 01:01:48 -0500

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Vincent Nguyen on 10/21/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
